fed94744ece2a55b85a62435c83a161f8032e397
TarannumMalik <tarannum.malik@somaiya.edu>
2023-09-28 19:38:53 +0530

    Todo Application (Angular)

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+  taskArray = [{ taskName: 'task', isCompleted: false }];
+
+  constructor() { }
+  ngOnInit(): void { }
+
+  onsubmit(form: NgForm) {
+    console.log(form);
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
+
+    form.reset();
+  }
+
+  onDelete(index:number){
+console.log(index);
+this.taskArray.splice(index,1)
+
+  }
+  onCheck(index:number){
+    console.log(this.taskArray);
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+}
